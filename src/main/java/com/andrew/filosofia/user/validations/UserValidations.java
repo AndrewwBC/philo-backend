7ca4628009b58b4987be9b568c05047079143f13
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-07 17:45:44 -0300

    add(UserValidations): method updateValidate

--- src/main/java/com/andrew/filosofia/user/validations/UserValidations.java
+++ src/main/java/com/andrew/filosofia/user/validations/UserValidations.java
@@ -1,7 +1,9 @@
 package com.andrew.filosofia.user.validations;
 
+import com.andrew.filosofia.user.User;
 import com.andrew.filosofia.user.dto.UserDTO;
 
 public interface UserValidations {
     void signInValidate(UserDTO userDTO);
+    void updateValidate(User user);
 }
