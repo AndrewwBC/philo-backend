2bbaa747fc6d419f2f31e3df497fa2f0594b4979
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-07 18:06:58 -0300

    add(UserValidate): incomplete updateValidate

--- src/main/java/com/andrew/filosofia/user/validations/UserValidate.java
+++ src/main/java/com/andrew/filosofia/user/validations/UserValidate.java
@@ -1,12 +1,15 @@
 package com.andrew.filosofia.user.validations;
 
 import com.andrew.filosofia.exception.exceptions.user.ValidateException;
+import com.andrew.filosofia.user.User;
 import com.andrew.filosofia.user.dto.UserDTO;
 import com.andrew.filosofia.user.UserRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 @Component
 public class UserValidate implements UserValidations {
@@ -40,7 +43,22 @@ public class UserValidate implements UserValidations {
     @Override
     public void updateValidate(UserDTO userDTO, String id) {
 
+        User user = this.userRepository.findById(id).orElseThrow(() ->
+                new NoSuchElementException("Usuário não encontrado!"));
 
+        String oldUsername = user.getUsername();
+        String oldEmail = user.getEmail();
+
+        String newUsername = userDTO.username();
+        String newEmail = userDTO.email();
+
+        if(!Objects.equals(oldUsername, newUsername)) {
+            boolean newUsernameAlreadyExists = this.userRepository.existsByUsername(newUsername);
+        }
+
+        if(!Objects.equals(oldEmail, newEmail)) {
+            boolean newEmailAlreadyExists = this.userRepository.existsByEmail(newEmail);
+        }
 
     }
 
