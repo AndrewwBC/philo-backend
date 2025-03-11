0bfafcc09410fa3331deb75f3f2bf87eb835e72f
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-11 04:36:57 -0300

    add(PostService): created PostService with dp injection already

---
+++ src/main/java/com/andrew/filosofia/post/PostService.java
@@ -0,0 +1,9 @@
+package com.andrew.filosofia.post;
+
+public class PostService {
+    PostRepository postRepository;
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+}
