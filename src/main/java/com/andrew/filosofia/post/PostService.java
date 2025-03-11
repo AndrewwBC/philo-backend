4bb21ee84ed838047dd710beed7850163c6fd3c6
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-11 04:52:03 -0300

    add(PostService): user repository property

--- src/main/java/com/andrew/filosofia/post/PostService.java
+++ src/main/java/com/andrew/filosofia/post/PostService.java
@@ -1,11 +1,15 @@
 package com.andrew.filosofia.post;
 
 import com.andrew.filosofia.post.dto.PostDTO;
+import com.andrew.filosofia.user.User;
+import com.andrew.filosofia.user.UserRepository;
 
 public class PostService {
     PostRepository postRepository;
-    public PostService(PostRepository postRepository) {
+    UserRepository userRepository;
+    public PostService(PostRepository postRepository, UserRepository userRepository) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
     }
 
 
