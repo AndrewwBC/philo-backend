2e16ba605944bcfe06f22176b7be9bcead319111
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-11 04:35:12 -0300

    add(PostRepository): create postRepo

--- src/main/java/com/andrew/filosofia/post/PostController.java
+++ src/main/java/com/andrew/filosofia/post/PostController.java
@@ -1,15 +1,17 @@
 package com.andrew.filosofia.post;
 
+import com.andrew.filosofia.post.dto.PostDTO;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/post")
 public class PostController {
-
 
 
 }
