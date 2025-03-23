b4df64d3a4db055b78fabda8e68e32649eef6713
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-22 23:33:55 -0300

    feat(AuthService) signin service

--- src/main/java/com/andrew/filosofia/auth/AuthService.java
+++ src/main/java/com/andrew/filosofia/auth/AuthService.java
@@ -1,12 +1,29 @@
 package com.andrew.filosofia.auth;
 
+import com.andrew.filosofia.auth.dto.SignInDTO;
+import com.andrew.filosofia.infra.TokenService;
+import com.andrew.filosofia.user.User;
 import com.andrew.filosofia.user.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
-    UserRepository userRepository;
-    public AuthService(UserRepository userRepository) {
+    private final UserRepository userRepository;
+    private final AuthenticationManager authManager;
+    private final TokenService tokenService;
+
+    public AuthService(AuthenticationManager authManager,TokenService tokenService,UserRepository userRepository) {
+        this.authManager = authManager;
+        this.tokenService = tokenService;
         this.userRepository = userRepository;
     }
+    public String signin(SignInDTO signInDTO) {
+        var usernamePass = new UsernamePasswordAuthenticationToken(signInDTO.email(), signInDTO.password());
+        var auth = this.authManager.authenticate(usernamePass);
+
+        return this.tokenService.generateToken((User) auth.getPrincipal());
+    }
+
 }
